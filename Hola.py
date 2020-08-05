a01e82ed332bfbd089908a8e976d7b283060d575
exequielvial <38020602+exequielvial@users.noreply.github.com>
2020-08-05 14:54:18 -0400

    + Hola.py

---
+++ Hola.py
@@ -0,0 +1,24 @@
+name = 'Exequiel'
+day = 5
+
+print ('Hola Exequiel es el 5 de agosto')
+print('\n')
+
+print ('Forma 2')
+print('Hola ' + name + ' es el ' + str(day) + ' de Agosto')
+print('\n')
+
+print('Forma 3')
+print('Hola {name} es el {day} de Agosto'.format(day=day, name=name))
+print('\n')
+
+print('Forma 4')
+print(f'Hola {name} es el {day} de Agosto')
+print('\n')    
+
+print('Ejemplo 2 - Forma 1')
+print('Hola Hola Hola Hola Hola')
+print('\n')
+
+print('Ejemplo 2 - Forma 2')
+print('Hola '*5)
