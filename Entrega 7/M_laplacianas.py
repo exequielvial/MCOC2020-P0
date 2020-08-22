a97391ee992284474354b94c8db37a5e0db424a0
exequielvial <38020602+exequielvial@users.noreply.github.com>
2020-08-21 22:22:57 -0400

    Add files via upload

--- Entrega 7/M_laplacianas.py
+++ Entrega 7/M_laplacianas.py
@@ -9,27 +9,13 @@ from scipy.sparse import lil_matrix
 import numpy as np
 
 def matriz_laplaciana_llena(N, t=np.float32):
-#    A = np.identity(N,t) * 2
- #   for i in range (N):
-  #      for j in range (N):
-   #         if i + 1 == j:
-    #            A[i,j] = -1
-     #       if i - 1 == j:
-      #          A[i,j] = -1 '''
+
     e = np.eye(N) - np.eye(N,N,1)
     
     return t(e+e.T)
 
 def matriz_laplaciana_dispersa(N, t=np.float32):
-#   A = lil_matrix((N,N))
- #   for i in range (N):
-  #      for j in range(N):
-   #         if i == j:
-    #            A[i,j] = 2
-     #       if i + 1 == j:
-      #          A[i,j] = -1
-       #     if i - 1 == j:
-        #        A[i,j] = -1'''
+
     e = eye(N)-eye(N,N,1)
     
     return t(e+e.T)
